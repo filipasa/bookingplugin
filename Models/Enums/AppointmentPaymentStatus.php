4d45cf7709e988495d901c73a44defd764770bb1
filipasa <110625542+filipasa@users.noreply.github.com>
2025-07-18 15:14:45 +0100

    Add files via upload

---
+++ Models/Enums/AppointmentPaymentStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BookneticApp\Models\Enums;
+
+class AppointmentPaymentStatus {
+    const PAID = 'paid';
+
+    const NOT_PAID = 'not_paid';
+
+    const PENDING = 'pending';
+
+    const CANCELED = 'canceled';
+}
